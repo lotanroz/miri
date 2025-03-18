67febbdd216511b5895cc4025bc79d44f49ec26a
lotanroz <theredroz2006@gmail.com>
2025-03-18 18:07:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // דוגמת מערך הצמחים
 const plants = [
-    { name: "מונסטרה", water: 2, difficulty: "בינוני", image: "https://images.unsplash.com/photo-1614594975525-e45190c55d0b?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
-    { name: "אנטוריום", water: 3, difficulty: "בינוני", image: "https://images.unsplash.com/photo-1607334386016-091a546f457d?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
-    { name: "פוטוס", water: 1, difficulty: "קל", image: "https://images.unsplash.com/photo-1605966706128-927ad2c9e2c8?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }
+    { name: "מונסטרה", water: 2, difficulty: 2, image: "https://images.unsplash.com/photo-1614594975525-e45190c55d0b?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { name: "אנטוריום", water: 3, difficulty: 3, image: "https://images.unsplash.com/photo-1607334386016-091a546f457d?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { name: "פוטוס", water: 1, difficulty: 1, image: "https://images.unsplash.com/photo-1605966706128-927ad2c9e2c8?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }
 ];
 
 // פונקציה לחישוב והצגת רמת השקייה
@@ -26,6 +26,27 @@ function createWaterDrops(level) {
     return container;
 }
 
+// פונקציה לחישוב והצגת מד הקושי
+function createDifficultyDrops(level) {
+    const container = document.createElement("div");
+    container.classList.add("difficulty-level");
+
+    for (let i = 1; i <= 3; i++) {
+        const difficulty = document.createElement("div");
+        difficulty.classList.add("difficulty");
+
+        if (level < i) {
+            difficulty.classList.add("empty");
+        } else {
+            difficulty.classList.add("full");
+        }
+
+        container.appendChild(difficulty);
+    }
+
+    return container;
+}
+
 // יצירת כרטיסי הצמחים
 const catalog = document.getElementById("plant-catalog");
 
@@ -43,6 +64,10 @@ plants.forEach(plant => {
     // הוספת רמות השקייה לכרטיס הצמח
     const wateringDrops = createWaterDrops(plant.water);
     plantCard.appendChild(wateringDrops);
+
+    // הוספת מד הקושי
+    const difficultyDrops = createDifficultyDrops(plant.difficulty);
+    plantCard.appendChild(difficultyDrops);
 
     plantCard.innerHTML += `
         <p>רמת קושי: ${plant.difficulty}</p>
